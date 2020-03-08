1187fe1675fa81dac72de75985a5e47c17a6bb5f
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-03-09 11:19:19 +1300

    Initial commit:     Setup Spring Boot first. Comment out jdbc from pom.xml - temporarily.

---
+++ src/test/java/com/paomanz/studentregistry/StudentregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paomanz.studentregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
