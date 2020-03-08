5dff6a252d8369436e66850f824546a57bc52b85
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-03-09 12:05:23 +1300

    sec-1-5:   Annotate Student Class with @RestController and @RequestMapping() - tells SpringBoot what this class served as.   Annotate method @GetMapping if method is served as a GET-Request query.

--- src/main/java/com/paomanz/studentregistry/student/StudentController.java
+++ src/main/java/com/paomanz/studentregistry/student/StudentController.java
@@ -1,10 +1,19 @@
 package com.paomanz.studentregistry.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 import java.util.UUID;
 
+// Tell SpringBoot this Class will be served as a REST-controller:
+// specify the Mapping with "" - double-quotes
+@RestController
+@RequestMapping("student")
 public class StudentController {
-
+   // Tell SpringBoot this method will serve as a GET-Req by Annotating:
+   @GetMapping
    public List<Student> getAllStudents() {
       // TEMPORARY: hard-coded data
       return List.of(
