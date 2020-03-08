36a5f80bdbea8a74f8fb3597d7b13191a47690f1
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-03-09 11:45:47 +1300

    Create and Define Model: Student.

---
+++ src/main/java/com/paomanz/studentregistry/student/Student.java
@@ -0,0 +1,48 @@
+package com.paomanz.studentregistry.student;
+
+import java.util.UUID;
+
+public class Student {
+   private final UUID studentId;
+   private final String firstName;
+   private final String lastName;
+   private final String email;
+   private final String gender;
+
+   public Student(UUID studentId,
+                  String firstName,
+                  String lastName,
+                  String email,
+                  String gender) {
+      this.studentId = studentId;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.gender = gender;
+   }
+
+   // Getters only, cos all Props are 'final'
+   public UUID getStudentId() {
+      return studentId;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public String getGender() {
+      return gender;
+   }
+
+   enum Gender {
+      MALE, FEMALE
+   }
+}
