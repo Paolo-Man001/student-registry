3e7d18df739eba1c3001b33d05d4fce8598c9974
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-03-22 22:28:00 +1300

    s7.50 : Custom Exceptions and Handlers :   Delete the 'Throwable' Object of the Exception. Delete all of its getter and variables instances from the other 2 exception files.   We don't have a use for it,and since we only need the TimeStamp, Message, and httpStatus.

--- src/main/java/com/paomanz/studentregistry/exception/ApiException.java
+++ src/main/java/com/paomanz/studentregistry/exception/ApiException.java
@@ -5,18 +5,17 @@ import org.springframework.http.HttpStatus;
 import java.time.ZonedDateTime;
 
 public class ApiException {
-   private final String message;
 //   private final Throwable throwable;
+
+   private final String message;
    private final HttpStatus httpStatus;
    private final ZonedDateTime timeStamp;
 
    public ApiException(
            String message,
-           Throwable throwable,
            HttpStatus httpStatus,
            ZonedDateTime timeStamp) {
       this.message = message;
-//      this.throwable = throwable;
       this.httpStatus = httpStatus;
       this.timeStamp = timeStamp;
    }
@@ -25,9 +24,6 @@ public class ApiException {
       return message;
    }
 
-//   public Throwable getThrowable() {
-//      return throwable;
-//   }
 
    public HttpStatus getHttpStatus() {
       return httpStatus;
