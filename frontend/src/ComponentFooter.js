d5c1b5215b531158b455f886372c7b85530c03d2
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-03-22 20:48:02 +1300

    s7.49 : add doc in ComponentFooter.js ternary condition

--- frontend/src/ComponentFooter.js
+++ frontend/src/ComponentFooter.js
@@ -7,7 +7,7 @@ const ComponentFooter = ( props ) => (
     <div>
        <Container fluid className="footer text-center">
           {
-             props.numberOfStudents !== undefined ?
+             props.numberOfStudents !== undefined ? // changed to 'if undefined' to make '0' invalid too, and returns 'null'
                  <Avatar
                      style={ { backgroundColor: '#f56a00', marginRight: '5px' } }
                      size='large'>
